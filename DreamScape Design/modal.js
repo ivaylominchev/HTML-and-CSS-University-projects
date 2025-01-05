f5a1eb74f63bb8392b481e2fbb203334e2e3409a
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2025-01-05 14:17:10 +0200

    add js and fixing css

--- DreamScape Design/modal.js
+++ DreamScape Design/modal.js
@@ -13,3 +13,50 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    const galleryImages = document.querySelectorAll('.gallery-image-item');
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+    const closeModal = document.getElementById('close-modal');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+
+    let currentImageIndex = 0;
+
+    
+    galleryImages.forEach((img, index) => {
+        img.addEventListener('click', () => {
+            currentImageIndex = index;
+            openModal(img.src);
+        });
+    });
+
+    
+    function openModal(src) {
+        modal.style.display = 'block';
+        modalImage.src = src;
+    }
+
+    
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    
+    nextButton.addEventListener('click', () => {
+        currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
+        modalImage.src = galleryImages[currentImageIndex].src;
+    });
+
+    // Навигация назад
+    prevButton.addEventListener('click', () => {
+        currentImageIndex = (currentImageIndex - 1 + galleryImages.length) % galleryImages.length;
+        modalImage.src = galleryImages[currentImageIndex].src;
+    });
+
+    
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
